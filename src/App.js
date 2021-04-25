9c47f36052f57ef50747b3dfaddbd7474b48855e
ali.zaidi <msazaidi@gmail.com>
2021-04-24 19:47:20 -0700

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import NoteList from './Components/Note/NoteList'
 import CreateNote from './Components/Note/CreateNote';
