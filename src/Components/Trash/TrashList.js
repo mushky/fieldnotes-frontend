81d89c7569ef9408f87683aadb9f5a4a1ca3d435
ali.zaidi <msazaidi@gmail.com>
2021-04-27 17:07:19 -0700

    Complete Trash Component  (Soft Delete)

---
+++ src/Components/Trash/TrashList.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../../Context/UserContext';
+
+import { Link } from 'react-router-dom';
+
+import SmallNote from '../Note/SmallNote';
+
+import axios from 'axios';
+
+const TrashList = () => {
+	const [ notes, setNotes ] = useState([]);
+	const { userValue } = useContext(UserContext);
+
+	const [ selectedNote, setSelectedNote ] = useState({
+		_id: "",
+		title: "",
+		content: "",
+		link: "",
+		category: "",
+		tags: "",
+		userId: userValue[0],
+		isTrash: ""
+	})
+	
+	const [ loading, setLoading ] = useState(false);
+	
+	
+	const url = `http://localhost:3001/api`
+
+	const userId = userValue[0];
+	const token = userValue[3];
+
+	const headers = {
+		"x-access-token": token
+	}
+  
+	const fetchNotes = async () => {
+		setLoading(true);
+		const res = await axios.get(`${url}/notes/user/${userValue[0]}`)
+		setNotes(res.data.Note.filter(note => note.isTrash))
+		setLoading(false);
+	}
+
+	useEffect(() => {
+		fetchNotes();
+	},[])
+
+	const selectNote = (noteObject) => {
+		setSelectedNote({
+			id: noteObject.id,
+			title: noteObject.title,
+			content: noteObject.content,
+			link: noteObject.link,
+			category: noteObject.category,
+			tags: noteObject.tags,
+			userId: noteObject.userId
+		});
+
+		setTimeout(() => {
+			fetchNotes();
+		},200);
+	
+	}
+
+
+	return(
+		<div className="container-trash">
+			<h2>Trash List</h2>
+			<br></br>
+			<Link to="/Notes">Back</Link>
+			{ loading && <h1>Loading...</h1> }
+			{notes.map((noteItem) => {
+				return(
+					<div>
+
+						{/* Show on Mobile */}
+
+						<div className="responsive-note-detail-view">
+							<SmallNote 
+								key={noteItem._id} id={noteItem._id} title={noteItem.title} 
+								content={noteItem.content} link={noteItem.link} category={noteItem.category}
+								tags={noteItem.tags} userId={noteItem.userId} isTrash={noteItem.isTrash} onSelect={selectNote}
+							/>
+
+
+						</div>
+
+						{/* Show on Desktop */}
+
+						<div className="note-detail-view">
+							<SmallNote 
+								key={noteItem._id} id={noteItem._id} title={noteItem.title} 
+								content={noteItem.content} link={noteItem.link} category={noteItem.category}
+								tags={noteItem.tags} userId={noteItem.userId} isTrash={noteItem.isTrash} onSelect={selectNote}
+							/>
+
+						</div>
+
+					</div>
+				)
+			})}
+		</div>
+	)
+
+}
+
+export default TrashList;
