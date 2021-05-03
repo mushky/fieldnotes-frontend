890143ecf3d0d1e8eaa9a9f416300966c8ac7c49
ali.zaidi <msazaidi@gmail.com>
2021-05-02 18:26:37 -0700

    fix warnings on svg elements

--- src/Components/Note/EditNote.js
+++ src/Components/Note/EditNote.js
@@ -123,8 +123,8 @@ const EditNote = (props) => {
 						width="4em" height="4em" 
 						viewBox="0 0 24 24">
 						<g fill="none">
-							<path d="M16.474 5.408l2.118 2.117m-.756-3.982L12.109 9.27a2.118 2.118 0 0 0-.58 1.082L11 13l2.648-.53c.41-.082.786-.283 1.082-.579l5.727-5.727a1.853 1.853 0 1 0-2.621-2.621z" stroke="#01c352" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-							<path d="M19 15v3a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h3" stroke="#01c352" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M16.474 5.408l2.118 2.117m-.756-3.982L12.109 9.27a2.118 2.118 0 0 0-.58 1.082L11 13l2.648-.53c.41-.082.786-.283 1.082-.579l5.727-5.727a1.853 1.853 0 1 0-2.621-2.621z" stroke="#01c352" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+							<path d="M19 15v3a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h3" stroke="#01c352" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 						</g>
 					</svg>					
 				</div>
